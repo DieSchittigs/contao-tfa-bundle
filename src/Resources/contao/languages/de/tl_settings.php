aa8a32a8f2bb832a91980744f29e1b6cfb80684c
Christopher Mühl <christopher@dieschittigs.de>
2018-01-05 11:04:48 +0100

    Change language texts

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][0] = 'Erlaubte 2FA-Diskrepanz';
-$GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][1] = 'Gibt an wie viele Perioden der eingegebene 2FA-Code vom Server-Code abweichen kann (z.B. 1 erlaubt eine Abweichung von &plusmn;30 Sekunden, 2 erlaubt &plusmn;60 Sekunden, &hellip;)';
+$GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][1] = 'Gibt an wie viele Perioden der eingegebene 2FA-Code vom Server-Code abweichen kann (1 erlaubt eine Abweichung von &plusmn;30 Sekunden, 2 erlaubt &plusmn;60 Sekunden, &hellip;).';
--- src/Resources/contao/languages/en/tl_settings.php
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][0] = 'Allowed 2FA discrepancy';
-$GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][1] = 'Specifies how many periods the entered 2FA code can deviate from the server-code (e.g. 1 allows for a deviation of &plusmn;30 seconds, 2 allows for &plusmn;60 seconds, &hellip;)';
+$GLOBALS['TL_LANG']['tl_settings']['tfaTtopDiscrepancy'][1] = 'Specifies how many periods the entered 2FA code can deviate from the server-code (1 allows for a deviation of &plusmn;30 seconds, 2 allows for &plusmn;60 seconds, &hellip;).';
