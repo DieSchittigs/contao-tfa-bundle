cc51204a97bead9b3c5c2042f0840cb4d2e63e76
Christopher Mühl <christopher@dieschittigs.de>
2018-01-05 09:46:49 +0100

    Move language variables from modules to tl_user

--- src/Resources/contao/languages/de/tl_user.php
+++ src/Resources/contao/languages/de/tl_user.php
@@ -2,3 +2,10 @@
 
 $GLOBALS['TL_LANG']['tl_user']['tfa_title'] = 'Zwei-Faktor-Authentifizierung';
 $GLOBALS['TL_LANG']['tl_user']['tfa_exception_invalid'] = 'Der eingegebene Code ist nicht gültig.';
+
+$GLOBALS['TL_LANG']['tl_user']['tfa_deactivate'] = 'Zwei-Faktor-Authentifizierung deaktivieren';
+$GLOBALS['TL_LANG']['tl_user']['tfa_deactivate_explanation'] = 'Zur Bestätigung bitte rechts den aktuellen Code eingeben.';
+
+$GLOBALS['TL_LANG']['tl_user']['tfa_explanation'] = 'Um die Zwei-Faktor-Authentifizierung für Ihren Account zu aktivieren, scannen Sie bitte diesen QR-Code mit einer kompatiblen App wie dem <strong>Google Authenticator</strong> (verfügbar für iOS und Android) und geben Sie danach den generierten Code ein.';
+$GLOBALS['TL_LANG']['tl_user']['tfa_activated'] = 'Sie haben die Zwei-Faktor-Authentifizierung bereits aktiviert. Bitte halten Sie Ihre Authenticator-App bereit, wenn Sie sich zukünftig in Contao einloggen.';
+$GLOBALS['TL_LANG']['tl_user']['tfa_token'] = 'Von App generierter Code';
